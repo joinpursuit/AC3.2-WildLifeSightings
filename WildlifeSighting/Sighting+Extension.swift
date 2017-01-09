73295c79b38e9b7600e9d7d7e244649873351a13
Tom Seymour <tomeseymour44@gmail.com>
2017-01-09 17:29:39 -0500

    added core data entities and delegate methods to main VC

---
+++ WildlifeSighting/Sighting+Extension.swift
@@ -0,0 +1,40 @@
+//
+//  Sighting+Extension.swift
+//  WildlifeSighting
+//
+//  Created by Tom Seymour on 1/9/17.
+//  Copyright © 2017 Sabrina, Vic, Tom. All rights reserved.
+//
+
+import Foundation
+
+extension Sighting {
+    
+    var dateAndTime: String {
+        let formatter = DateFormatter()
+        formatter.dateStyle = .long
+        formatter.timeStyle = .medium
+        return formatter.string(from: date as! Date)
+    }
+    
+    var time: String {
+        let formatter = DateFormatter()
+        formatter.dateStyle = .none
+        formatter.timeStyle = .medium
+        return formatter.string(from: date as! Date)
+    }
+    
+    var dateString: String {
+        let date = self.date!
+        let formatter = DateFormatter()
+        formatter.dateStyle = .long
+        let objectDateString = formatter.string(from: date as Date)
+        let todayDateString = formatter.string(from: NSDate() as Date)
+        if objectDateString == todayDateString {
+            return "Today"
+        }
+        return objectDateString
+    }
+}
+
+
