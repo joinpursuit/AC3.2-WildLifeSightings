ad1723615af8f8c09e29b9a670ebe8f63d5dcc98
Tom Seymour <tomeseymour44@gmail.com>
2017-01-12 11:03:28 -0500

    fixed sort bug

--- WildlifeSighting/DarkSkiesWeather.swift
+++ WildlifeSighting/DarkSkiesWeather.swift
@@ -44,57 +44,6 @@ class DarkSkiesWeather {
         catch {
             print(error)
         }
-        
         return nil
     }
-    
-    //    static func getWeather (from data: Data) -> DarkSkiesWeather? {
-    //
-    //        do {
-    //            let jsonData: Any = try JSONSerialization.jsonObject(with: data, options: [])
-    //            guard let response = jsonData as? [ String : Any ] else { throw DarkSkiesModelError.response }
-    //            guard let result = response["currently"] as? [ String : Any ] else { throw DarkSkiesModelError.result }
-    //            guard let summary = result["summary"] as? String else { throw DarkSkiesModelError.summary }
-    //            guard let temperature = result["temperature"] as? Double else { throw DarkSkiesModelError.temperature }
-    //
-    //            return DarkSkiesWeather(summary:summary, temp:temperature)
-    //
-    //        }
-    //
-    //        catch {
-    //            print(error)
-    //        }
-    //
-    //        return nil
-    //    }
-    
-    
 }
-
-
-/*
- {
- "latitude": 37.8267,
- "longitude": -122.4233,
- "timezone": "America/Los_Angeles",
- "offset": -8,
- "currently": {
- "time": 1483999581,
- "summary": "Mostly Cloudy",
- "icon": "partly-cloudy-day",
- "nearestStormDistance": 10,
- "nearestStormBearing": 357,
- "precipIntensity": 0,
- "precipProbability": 0,
- "temperature": 56.42,
- "apparentTemperature": 56.42,
- "dewPoint": 49.13,
- "humidity": 0.77,
- "windSpeed": 10.97,
- "windBearing": 222,
- "visibility": 7.88,
- "cloudCover": 0.93,
- "pressure": 1018.13,
- "ozone": 303.6
- },
- */
